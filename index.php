dbbdd76e7ed21b355df573c818f2f50f391f999b
Piotr Tomaszkiewicz <piotr.tomaszkiewicz@live.zs1mm.edu.pl>
2022-09-13 10:00:11 +0200

    prawdopodobnie3.1i3.2

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretium turpis vel nisi dapibus, eget gravida urna maximus. Vivamus faucibus accumsan orci. Ut pulvinar aliquam lorem quis suscipit. Nunc est velit, condimentum sed nisi ac, egestas rhoncus nulla. Cras nec nisi nec neque ultrices vestibulum. Morbi eu libero gravida, aliquet sapien id, mollis nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin risus metus, feugiat quis felis quis, laoreet imperdiet turpis. Proin vulputate ultricies sem, vel tincidunt libero porttitor at. Nam eu mattis sapien, ac malesuada nisi. Aliquam condimentum, velit semper auctor interdum, enim.";
 
-$search = "o";
+$search = "";
 function sortandsearch($text, $look){
     $words = explode(" ", $text);
 $i = 1;
@@ -15,4 +15,26 @@ foreach($words as $word){
     }
 }
 };
-sortandsearch($ipsum, $search);
+// sortandsearch($ipsum, $search);
+$words = explode(" ", $ipsum);
+$e = 1;
+echo "<table>";
+foreach($words as $word){
+    if($e<=4){
+    if ($e%4==1){
+        echo "<tr><th>$word</th>";
+    } else if ($e%4==0){
+        echo "<th>$word</th></tr>";
+    } else {
+        echo "<th>$word</th>";
+    }} else {
+    if ($e%4==1){
+        echo "<tr><td>$word</td>";
+    } else if ($e%4==0){
+        echo "<td>$word</td></tr>";
+    } else {
+        echo "<td>$word</td>";
+    }}
+    $e ++;
+}
+echo "</table>";
