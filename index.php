ae0a726b93db9650aa69ba9f6edf90d7e83b645c
Piotr Tomaszkiewicz <piotr.tomaszkiewicz@live.zs1mm.edu.pl>
2022-09-13 09:23:42 +0200

    zad2.2i2.4ButAsFunction

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 $ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretium turpis vel nisi dapibus, eget gravida urna maximus. Vivamus faucibus accumsan orci. Ut pulvinar aliquam lorem quis suscipit. Nunc est velit, condimentum sed nisi ac, egestas rhoncus nulla. Cras nec nisi nec neque ultrices vestibulum. Morbi eu libero gravida, aliquet sapien id, mollis nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin risus metus, feugiat quis felis quis, laoreet imperdiet turpis. Proin vulputate ultricies sem, vel tincidunt libero porttitor at. Nam eu mattis sapien, ac malesuada nisi. Aliquam condimentum, velit semper auctor interdum, enim.";
-$words = explode(" ", $ipsum);
+
+$search = "o";
+function sortandsearch($text, $look){
+    $words = explode(" ", $text);
 $i = 1;
-$search = "";
+
 sort($words, SORT_NATURAL | SORT_FLAG_CASE);
 foreach($words as $word){
 
-    if(preg_match("/\b(\w*$search\w*)\b/", $word, $match) == true){
+    if(preg_match("/\b(\w*$look\w*)\b/", $word, $match) == true){
     echo "[$i] $match[0] </br>";
     $i ++;
     }
 }
+};
+sortandsearch($ipsum, $search);
