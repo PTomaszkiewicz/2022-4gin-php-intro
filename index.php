c863000f67ac8f564f5daf725262f58c95a1d5bc
Piotr Tomaszkiewicz <piotr.tomaszkiewicz@live.zs1mm.edu.pl>
2022-09-20 19:35:44 +0200

    zad5

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <?php
 $ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretium turpis vel nisi dapibus, eget gravida urna maximus. Vivamus faucibus accumsan orci. Ut pulvinar aliquam lorem quis suscipit. Nunc est velit, condimentum sed nisi ac, egestas rhoncus nulla. Cras nec nisi nec neque ultrices vestibulum. Morbi eu libero gravida, aliquet sapien id, mollis nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin risus metus, feugiat quis felis quis, laoreet imperdiet turpis. Proin vulputate ultricies sem, vel tincidunt libero porttitor at. Nam eu mattis sapien, ac malesuada nisi. Aliquam condimentum, velit semper auctor interdum, enim.";
-
 $search = "";
+$collumn = 5;
+//Sort text by search phrase and list it
 function sortandsearch($text, $look){
     $words = explode(" ", $text);
 $i = 1;
-
 sort($words, SORT_NATURAL | SORT_FLAG_CASE);
 foreach($words as $word){
-
     if(preg_match("/\b(\w*$look\w*)\b/", $word, $match) == true){
     echo "[$i] $match[0] </br>";
     $i ++;
     }
 }
 };
-// sortandsearch($ipsum, $search);
-$collumn = 5;
+//Convert text to a HTML table
 function renderHTMLTable($text2, $cl){
     $words = explode(" ", $text2);
     $e = 1;
@@ -42,6 +40,7 @@ function renderHTMLTable($text2, $cl){
     $table .= "</table>";
     echo $table;
 };
+//Render table.csv table out of given text
 function rednderCSV($text2, $cl){
     ob_start();
     renderHTMLTable($text2, $cl);
@@ -71,6 +70,7 @@ if(!fwrite($handle, $csv)){
 echo "error2";};
 fclose($handle);
 }
+//Render table.md table out of given text
 function renderMD($text2, $cl){
     ob_start();
     renderHTMLTable($text2, $cl);
@@ -82,13 +82,10 @@ preg_match_all('/<tr(>| [^>]*>)(.*?)<\/tr( |>)/is',$table,$b);
 $rows = $b[2];
 foreach ($rows as $row) {
     //cycle through each row
-    if(preg_match('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row)) {
-        
+    if(preg_match('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row)) {      
         //match for table headers
         preg_match_all('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row,$b);
-        
         $csv[] = strip_tags(implode('|',$b[2]));
-
     } elseif(preg_match('/<td(>| [^>]*>)(.*?)<\/td( |>)/is',$row)) {
         //match for table cells
         preg_match_all('/<td(>| [^>]*>)(.*?)<\/td( |>)/is',$row,$b);
@@ -108,4 +105,4 @@ if(!fwrite($handle, $csv)){
 echo "error2";};
 fclose($handle);
 }
-renderMD($ipsum, $collumn);
+?>
