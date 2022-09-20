a41b4c9130d6368e0e5d55ebbbd7b02d0e0f030c
Piotr Tomaszkiewicz <piotr.tomaszkiewicz@live.zs1mm.edu.pl>
2022-09-20 19:27:54 +0200

    zad4.2

--- index.php
+++ index.php
@@ -71,4 +71,41 @@ if(!fwrite($handle, $csv)){
 echo "error2";};
 fclose($handle);
 }
-rednderCSV($ipsum, $collumn);
+function renderMD($text2, $cl){
+    ob_start();
+    renderHTMLTable($text2, $cl);
+    $table = ob_get_clean();
+    $csv = array();
+preg_match('/<table(>| [^>]*>)(.*?)<\/table( |>)/is',$table,$b);
+$table = $b[2];
+preg_match_all('/<tr(>| [^>]*>)(.*?)<\/tr( |>)/is',$table,$b);
+$rows = $b[2];
+foreach ($rows as $row) {
+    //cycle through each row
+    if(preg_match('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row)) {
+        
+        //match for table headers
+        preg_match_all('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row,$b);
+        
+        $csv[] = strip_tags(implode('|',$b[2]));
+
+    } elseif(preg_match('/<td(>| [^>]*>)(.*?)<\/td( |>)/is',$row)) {
+        //match for table cells
+        preg_match_all('/<td(>| [^>]*>)(.*?)<\/td( |>)/is',$row,$b);
+        $csv[] = strip_tags(implode('|',$b[2]));
+    }
+}
+$csv[0]=$csv[0]."|\n";
+for($i=0;$i<$cl;$i++){
+$csv[0]= $csv[0]."|---";
+}
+$csv = implode("|\n|",$csv);
+$csv = "|".$csv."|";
+$file= "table.md";
+if(!$handle = fopen($file, "w")){
+    echo "error1";};
+if(!fwrite($handle, $csv)){
+echo "error2";};
+fclose($handle);
+}
+renderMD($ipsum, $collumn);
