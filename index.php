e334f8bc1faafbdb6dc264b799a6085e8a3c2ea8
Piotr Tomaszkiewicz <piotr.tomaszkiewicz@live.zs1mm.edu.pl>
2022-09-21 11:01:25 +0200

    Poprawki

--- index.php
+++ index.php
@@ -3,34 +3,34 @@ $ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretiu
 $search = "";
 $collumn = 5;
 //Sort text by search phrase and list it
-function sortandsearch($text, $look){
-    $words = explode(" ", $text);
-$i = 1;
-sort($words, SORT_NATURAL | SORT_FLAG_CASE);
-foreach($words as $word){
-    if(preg_match("/\b(\w*$look\w*)\b/", $word, $match) == true){
-    echo "[$i] $match[0] </br>";
-    $i ++;
+function sortandsearch($table, $lookingfor){
+    $output=array();
+    $words = explode(" ", $table);
+    sort($words, SORT_NATURAL | SORT_FLAG_CASE);
+    foreach($words as $word){
+        if(preg_match("/\b(\w*$lookingfor\w*)\b/", $word, $match) == true){
+            $output[]=$match[0];
+        }
     }
-}
+    return $output;
 };
 //Convert text to a HTML table
-function renderHTMLTable($text2, $cl){
-    $words = explode(" ", $text2);
+function renderHTMLTable($table, $lookingfor, $collumnsamount){
+    $words = sortandsearch($table, $lookingfor);
     $e = 1;
     $table= "<table>";
     foreach($words as $word){
-        if($e<=$cl){
-        if ($e%$cl==1){
+        if($e<=$collumnsamount){
+        if ($e%$collumnsamount==1){
             $table .= "<tr><th>$word</th>";
-        } else if ($e%$cl==0){
+        } else if ($e%$collumnsamount==0){
             $table .= "<th>$word</th></tr>";
         } else {
             $table .= "<th>$word</th>";
         }} else {
-        if ($e%$cl==1){
+        if ($e%$collumnsamount==1){
             $table .= "<tr><td>$word</td>";
-        } else if ($e%$cl==0){
+        } else if ($e%$collumnsamount==0){
             $table .= "<td>$word</td></tr>";
         } else {
             $table .= "<td>$word</td>";
@@ -38,71 +38,58 @@ function renderHTMLTable($text2, $cl){
         $e ++;
     }
     $table .= "</table>";
-    echo $table;
+    return $table;
 };
 //Render table.csv table out of given text
-function rednderCSV($text2, $cl){
-    ob_start();
-    renderHTMLTable($text2, $cl);
-    $table = ob_get_clean();
+function rednderCSV($table, $lookingfor, $collumnsamount){
+    
+    $htmltable = renderHTMLTable($table, $lookingfor, $collumnsamount);
     $csv = array();
-preg_match('/<table(>| [^>]*>)(.*?)<\/table( |>)/is',$table,$b);
-$table = $b[2];
-preg_match_all('/<tr(>| [^>]*>)(.*?)<\/tr( |>)/is',$table,$b);
-$rows = $b[2];
-foreach ($rows as $row) {
-    //cycle through each row
-    if(preg_match('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row)) {
-        //match for table headers
-        preg_match_all('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row,$b);
-        $csv[] = strip_tags(implode(';',$b[2]));
-    } elseif(preg_match('/<td(>| [^>]*>)(.*?)<\/td( |>)/is',$row)) {
-        //match for table cells
-        preg_match_all('/<td(>| [^>]*>)(.*?)<\/td( |>)/is',$row,$b);
-        $csv[] = strip_tags(implode(';',$b[2]));
+    preg_match('/<table(>| [^>]*>)(.*?)<\/table( |>)/is',$htmltable,$b);
+    $htmltable = $b[2];
+    preg_match_all('/<tr(>| [^>]*>)(.*?)<\/tr( |>)/is',$htmltable,$b);
+    $rows = $b[2];
+    foreach ($rows as $row) {
+        //cycle through each row
+        if(preg_match('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row)) {
+            //match for table headers
+            preg_match_all('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row,$b);
+            $csv[] = strip_tags(implode(';',$b[2]));
+        } elseif(preg_match('/<td(>| [^>]*>)(.*?)<\/td( |>)/is',$row)) {
+            //match for table cells
+            preg_match_all('/<td(>| [^>]*>)(.*?)<\/td( |>)/is',$row,$b);
+            $csv[] = strip_tags(implode(';',$b[2]));
+        }
     }
-}
-$csv = implode("\n", $csv);
-$file= "table.csv";
-if(!$handle = fopen($file, "w")){
-    echo "error1";};
-if(!fwrite($handle, $csv)){
-echo "error2";};
-fclose($handle);
+    $csv = implode("\n", $csv);
+    return $csv;
 }
 //Render table.md table out of given text
-function renderMD($text2, $cl){
-    ob_start();
-    renderHTMLTable($text2, $cl);
-    $table = ob_get_clean();
-    $csv = array();
-preg_match('/<table(>| [^>]*>)(.*?)<\/table( |>)/is',$table,$b);
-$table = $b[2];
-preg_match_all('/<tr(>| [^>]*>)(.*?)<\/tr( |>)/is',$table,$b);
-$rows = $b[2];
-foreach ($rows as $row) {
-    //cycle through each row
-    if(preg_match('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row)) {      
-        //match for table headers
-        preg_match_all('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row,$b);
-        $csv[] = strip_tags(implode('|',$b[2]));
-    } elseif(preg_match('/<td(>| [^>]*>)(.*?)<\/td( |>)/is',$row)) {
-        //match for table cells
-        preg_match_all('/<td(>| [^>]*>)(.*?)<\/td( |>)/is',$row,$b);
-        $csv[] = strip_tags(implode('|',$b[2]));
+function renderMD($table, $lookingfor, $collumnsamount){
+    $htmltable = renderHTMLTable($table, $lookingfor, $collumnsamount);
+    $md = array();
+    preg_match('/<table(>| [^>]*>)(.*?)<\/table( |>)/is',$htmltable,$b);
+    $htmltable = $b[2];
+    preg_match_all('/<tr(>| [^>]*>)(.*?)<\/tr( |>)/is',$htmltable,$b);
+    $rows = $b[2];
+    foreach ($rows as $row) {
+        //cycle through each row
+        if(preg_match('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row)) {      
+            //match for table headers
+            preg_match_all('/<th(>| [^>]*>)(.*?)<\/th( |>)/is',$row,$b);
+            $md[] = strip_tags(implode('|',$b[2]));
+        } elseif(preg_match('/<td(>| [^>]*>)(.*?)<\/td( |>)/is',$row)) {
+            //match for table cells
+            preg_match_all('/<td(>| [^>]*>)(.*?)<\/td( |>)/is',$row,$b);
+            $md[] = strip_tags(implode('|',$b[2]));
+        }
     }
-}
-$csv[0]=$csv[0]."|\n";
-for($i=0;$i<$cl;$i++){
-$csv[0]= $csv[0]."|---";
-}
-$csv = implode("|\n|",$csv);
-$csv = "|".$csv."|";
-$file= "table.md";
-if(!$handle = fopen($file, "w")){
-    echo "error1";};
-if(!fwrite($handle, $csv)){
-echo "error2";};
-fclose($handle);
+    $md[0]=$md[0]."|\n";
+    for($i=0;$i<$collumnsamount;$i++){
+        $md[0]= $md[0]."|---";
+    }
+    $md = implode("|\n|",$md);
+    $md = "|".$md."|";
+    return $md;
 }
 ?>
